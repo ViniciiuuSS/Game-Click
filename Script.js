75fec74dd0810fc961c63975952a7a2288441df8
Vinicius Guedes <vinicraft556@gmail.com>
2022-11-20 01:19:56 -0300

    Adicionando novas funcionalidades

--- Script.js
+++ Script.js
@@ -1,25 +1,62 @@
+let gV = {pHand: 1000};
 function Click_Main(){
     let Scores = +Score_value.value;
     Scores++;
     document.getElementById("Score_value").value = Scores
     document.getElementById("Score").value = document.getElementById("Score_value").value 
-    console.log(Score_value.value)
-}
-function Click_Up(){
-    let VScores = +VAuto_Score_value.value;
-    let AScores = +Score_value.value;
+    document.getElementById("Score_up").value = document.getElementById("Score_value").value 
 
-    if(AScores >= VScores){
-        AScores -= VScores
-        document.getElementById("Score_value").value = AScores
-        document.getElementById("Score").value = document.getElementById("Score_value").value 
-        document.getElementById("VAuto_Score_value").value = VScores + 25
-        
-        let Scores = +Auto_Score_value.value;
-        Scores++;
-        document.getElementById("Auto_Score_value").value = Scores
-        document.getElementById("Auto_Score").value = document.getElementById("Auto_Score_value").value 
-        console.log(VAuto_Score.value)
+    $('#sp_t').css({
+        'display' : 'flex',
+        'animation' : '1s trigger'
+    })
+    setTimeout(function () {
+        $('#sp_t').fadeOut()
+    },500)
+}
+function Click_Up(hand){
+    if (hand > 0 ){
+        for(let i = 0; i < hand; i++){
+            let VScores = +VAuto_Score_value.value;
+            let AScores = +Score_value.value;
+
+            if(AScores >= VScores){
+                AScores -= VScores
+                document.getElementById("Score_value").value = AScores
+                document.getElementById("Score").value = document.getElementById("Score_value").value 
+                document.getElementById("VAuto_Score_value").value = VScores + 25
+                
+                let Scores = +Auto_Score_value.value;
+                Scores++;
+                document.getElementById("Auto_Score_value").value = Scores
+                document.getElementById("Btn_UP").value = document.getElementById("Auto_Score_value").value 
+            }
+        }
+    }else{
+        let VScores = +VAuto_Score_value.value;
+        let AScores = +Score_value.value;
+
+        if(AScores >= VScores){
+            AScores -= VScores
+            document.getElementById("Score_value").value = AScores
+            document.getElementById("Score").value = document.getElementById("Score_value").value 
+            document.getElementById("VAuto_Score_value").value = VScores + 25
+            
+            let Scores = +Auto_Score_value.value;
+            Scores++;
+            document.getElementById("Auto_Score_value").value = Scores
+            document.getElementById("Btn_UP").value = document.getElementById("Auto_Score_value").value 
+        }
+    }   
+}
+function add_hand(){
+    let AScores = +Score_value.value;
+    if (AScores >= gV.pHand){
+        +hand_value.value++;
+        quantidade_hand.innerHTML = hand_value.value
+        Score_value.value = +Score_value.value - gV.pHand
+        gV.pHand += 5000
+        v_hand.innerHTML = gV.pHand 
     }
 }
 setInterval(function () {
@@ -27,7 +64,10 @@ setInterval(function () {
     let Scores = +Score_value.value + Scores_Auto;
     document.getElementById("Score_value").value = Scores
     document.getElementById("Score").value = document.getElementById("Score_value").value 
-    console.log(Score_value.value)
+    document.getElementById("Score_up").value = document.getElementById("Score_value").value 
+    if (+hand_value.value > 0){
+        Click_Up(+hand_value.value)
+    }
 },1000)
 function Tab(item){
     if (item ==1 ){
